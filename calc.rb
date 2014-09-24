39ce55bd7fd544a629e1e862c986a8a8ca17b859
areddy14 <areddy1441@gmail.com>
2014-09-23 20:06:14 -0400

    I finished phase 2. Had to use in-built method for sqrt

--- calc.rb
+++ calc.rb
@@ -27,6 +27,34 @@ def subtraction(num1, num2)
 	num1 - num2
 end
 
+def exponent(num1, num2)
+	puts num1.to_s + "^" + num2.to_s
+	multi_factor = num1
+	while num2>1
+		num1*= multi_factor
+		num2-=1
+	end
+	if num2 == 0
+		return 1
+	end
+	return num1
+end
+
+def square(num1)
+	puts num1.to_s + "^2"
+	num1 * num1
+end
+
+def cube(num1)
+	puts num1.to_s + "^3"
+	num1 * num1 * num1
+end
+
+def square_root(num1)
+	puts num1.to_s + "^(1/2)"
+	Math.sqrt(num1)
+end
+
 def print_output(answer)
 	puts answer
 end
@@ -62,6 +90,17 @@ elsif option == "multiply"
 elsif option == "divide"
 	result = divide(enter_first_number, enter_second_number)
 
+elsif option == "exponent"
+	result = exponent(enter_first_number, enter_second_number)
+
+elsif option == "square"
+	result = square(enter_first_number)
+
+elsif option == "cube"
+	result = cube(enter_first_number)
+
+elsif option == "square root"
+	result = square_root(enter_first_number)						
 end
 
 print_output(result)
